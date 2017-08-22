18c38612c56d39a6ea1ba9907e1db74e63827e66
matchawu <ruby05075151@hotmail.com>
2017-08-22 11:28:03 +0800

    application folder

---
+++ resources/views/application/form2.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.app')
+
+@section('title', 'form2')
+
+@section('content')
+
+	<main-page></main-page>
+@endsection
+
+@section('css')
+<script src="http://cdn.alloyui.com/3.0.1/aui/aui-min.js"></script>
+<link href="http://cdn.alloyui.com/3.0.1/aui-css/css/bootstrap.min.css" rel="stylesheet"></link>
+@endsection
+
+@section('js')
+<script>
+
+</script>
+@endsection
